7f2d6f620d6df351deebf067733ad7bb85373c0f
email <bethkimaku@gmail.com>
2023-06-11 07:19:41 +0300

    completed assignment

--- lib/models.py
+++ lib/models.py
@@ -9,6 +9,13 @@ convention = {
 metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
+game_user = Table(
+    'game_users',
+    Base.metadata,
+    Column('game_id', ForeignKey('games.id'), primary_key=True),
+    Column('user_id', ForeignKey('users.id'), primary_key=True),
+    extend_existing=True,
+)
 
 class Game(Base):
     __tablename__ = 'games'
@@ -20,11 +27,28 @@ class Game(Base):
     price = Column(Integer())
 
     reviews = relationship('Review', backref=backref('game'))
+    users = relationship('Review', backref=backref('game'), cascade='all, delete-orphan')
 
     def __repr__(self):
         return f'Game(id={self.id}, ' + \
             f'title={self.title}, ' + \
             f'platform={self.platform})'
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+
+    games = relationship('Game', secondary=game_user, back_populates='users')
+    reviews = relationship('Review', backref=backref('user'), cascade='all, delete-orphan')
+
+    def __repr__(self):
+        return f'User(id={self.id}, ' + \
+            f'name={self.name})'
+    
 
 class Review(Base):
     __tablename__ = 'reviews'
@@ -32,8 +56,9 @@ class Review(Base):
     id = Column(Integer(), primary_key=True)
     score = Column(Integer())
     comment = Column(String())
-    
+
     game_id = Column(Integer(), ForeignKey('games.id'))
+    user_id = Column (Integer(),ForeignKey('users.id'))
 
     def __repr__(self):
         return f'Review(id={self.id}, ' + \
